0df851c0a9701cf341dc5e683eb22698e15eff42
Josuu <164666532+Putrefacto669@users.noreply.github.com>
2025-11-26 20:01:23 -0600

    Add files via upload

---
+++ Capa Presentacion/reportes/RptEstadoCivil.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace CapaPresentación.Reportes
+{
+    public partial class RptEstadoCivil : DevExpress.XtraReports.UI.XtraReport
+    {
+        public RptEstadoCivil()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
