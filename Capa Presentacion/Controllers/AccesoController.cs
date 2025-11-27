9d06f3074fc32d43a4ae00ec1e842a0ea4a85bac
Josuu <164666532+Putrefacto669@users.noreply.github.com>
2025-11-26 19:38:08 -0600

    Create AccesoController.cs

---
+++ Capa Presentacion/Controllers/AccesoController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaPresentación.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
