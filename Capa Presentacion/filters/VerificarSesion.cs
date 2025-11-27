2fbb26fb7aad1eea9eafe51752baa971d588c42a
Josuu <164666532+Putrefacto669@users.noreply.github.com>
2025-11-26 19:58:20 -0600

    Add files via upload

---
+++ Capa Presentacion/filters/VerificarSesion.cs
@@ -0,0 +1,46 @@
+﻿using ProyectoWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaPresentación.Filters
+{
+    public class VerificarSesion : ActionFilterAttribute
+    {
+        private int? IdUsuario;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+
+
+
+                if (HttpContext.Current.Session["IdUsuario"] != null)
+                    IdUsuario = (int)HttpContext.Current.Session["IdUsuario"];
+                else
+                    IdUsuario = null;
+
+                if (IdUsuario == null)
+                {
+                    if (filterContext.Controller is LoginController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("/Login/Index");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                filterContext.Result = new RedirectResult("/Login/Index");
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+
+
+    }
+}
